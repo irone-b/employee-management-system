e7b398d8fdd2b016a05bc8e12d68049231d6b98a
Irone <ironebotha@gmail.com>
2025-10-26 18:31:50 +0200

    Added nav items and final layout changes

--- src/app/main-layout-shell/main-layout-shell.ts
+++ src/app/main-layout-shell/main-layout-shell.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { EmployeePayrollPage } from '../pages/payroll/employee-payroll-page/employee-payroll-page';
+import { AcNavItem } from '../shared/components/ac-nav-item/ac-nav-item';
 
 @Component({
   selector: 'main-layout-shell',
-  imports: [EmployeePayrollPage],
+  imports: [EmployeePayrollPage, AcNavItem],
   templateUrl: './main-layout-shell.html',
   styleUrl: './main-layout-shell.scss',
 })
